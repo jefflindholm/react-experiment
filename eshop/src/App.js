2d30bcf31d958f5d60fca1531938e99aa7050033
Jeff Lindholm <jeff@lindholm.org>
2016-08-04 10:59:09 -0400

    stuf

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,21 +1,41 @@
+// @flow
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, Navbar, Nav, NavItem, Jumbotron, Button } from 'react-bootstrap';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        const version = 1;
+        return (
+            <div>
+                <Navbar inverse fixedTop>
+                    <Grid>
+                        <Navbar.Header>
+                            <Navbar.Brand>
+                                <a href="/">React eShop {version}</a>
+                            </Navbar.Brand>
+                            <Navbar.Toggle />
+                        </Navbar.Header>
+                        <Navbar.Collapse>
+                            <Nav pullRight>
+                                <NavItem href="//github.com/manavsehgal/react-eshop">Original Code</NavItem>
+                                <NavItem href="//bitbucket.org/Team-Lindholm/experimental">My Code</NavItem>
+                                <NavItem href="//leanpub.com/reacteshop">Book</NavItem>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Grid>
+                </Navbar>
+                <Jumbotron>
+                    <Grid>
+                        <h1>Easily Reuse eShop in React</h1>
+                        <p>eShop written in React, ES6, Firebase</p>
+                        <p><Button bsStyle="success" bsSize="large">Learn More</Button></p>
+                    </Grid>
+                </Jumbotron>
+            </div>
+        );
+    }
 }
 
 export default App;
